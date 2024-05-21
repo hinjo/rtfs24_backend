6e9abff95bce94994ae07406224956a3d4f181bd
Vanja Ferhatovic <vanja.ferhatovic@gmail.com>
2024-05-21 15:53:18 +0200

    0.5 release

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+// variabler i api:er brukar ligga i db
+// const count = 0
+
 app.get("/", (_request, response) => {
   response.send({ hello: "World" })
+  // hämta från databas
+  // count++
+
   // response.send("<html><head></head><body>Hello there</body></html>")
 })
 
